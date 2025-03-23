51c1da92f6633c7bae0ebba2927ffe59fcaaf424
snehilsin <110235521+snehilsin@users.noreply.github.com>
2025-03-23 14:49:48 +0530

    Time: 0 ms (100%), Space: 29.4 MB (97.16%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -24,5 +24,33 @@ public:
         // [6, 2, 1, 3, 8, 7]
         // pura reverse
         // [7, 8, 3, 1, 2, 6]
+
+
+        /* Brute force using extra space
+
+        int n = nums.size();
+        if (k == n) return;
+
+        if (k > n){
+            k = k % n; // to ensure it is within the range [0..n]
+        }
+
+        vector<int> ans(n);
+        int j = 0;
+
+        // aakhiri ke k elements pehle le aao
+        for ( int i = n-k; i < n; i++){
+            ans[j++] = nums[i];
+        }
+
+        // bache first elements iske baad
+        for (int i = 0; i < n-k; i++){
+            ans[j++] = nums[i];
+        }
+
+        // finally nums ko modify kardo
+        for (int i = 0; i < n; i++){
+            nums[i] = ans[i];
+        }*/
     }
 };
