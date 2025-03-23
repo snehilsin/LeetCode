2d37f3168b39d8e04eafc933c00e1b774841f524
snehilsin <110235521+snehilsin@users.noreply.github.com>
2025-03-23 14:48:21 +0530

    Time: 0 ms (100%), Space: 29.5 MB (58.78%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        // Brute force approach using extra space
+        // reversal algo
 
         int n = nums.size();
-        if (k == n) return;
-
         if (k > n){
-            k = k % n; // to ensure it is within the range [0..n]
+            k = k % n;
         }
+        // aakhiri ke elements reverse kar
+        reverse(nums.begin() + (n-k), nums.end());
 
-        vector<int> ans(n);
-        int j = 0;
+        // bache hue first elements bhi reverse kar
+        reverse(nums.begin(), nums.begin() + n - k);
 
-        // aakhiri ke k elements pehle le aao
-        for ( int i = n-k; i < n; i++){
-            ans[j++] = nums[i];
-        }
+        // kuch mil gya na ab
+        // pura reverse kar
+        reverse(nums.begin(), nums.end());
 
-        // bache first elements iske baad
-        for (int i = 0; i < n-k; i++){
-            ans[j++] = nums[i];
-        }
-
-        // finally nums ko modify kardo
-        for (int i = 0; i < n; i++){
-            nums[i] = ans[i];
-        }
+        // [1, 2, 6, 7, 8, 3] , k = 3
+        // aakhiri ke k elements reverse
+        // [1, 2, 6, 3, 8, 7]
+        // bache hue first elements reverse
+        // [6, 2, 1, 3, 8, 7]
+        // pura reverse
+        // [7, 8, 3, 1, 2, 6]
     }
 };
