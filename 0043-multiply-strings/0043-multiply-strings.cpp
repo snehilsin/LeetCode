ec55e9a7e8b1e9208d2d23fcd7aea8f72d659897
snehilsin <110235521+snehilsin@users.noreply.github.com>
2025-03-25 02:14:50 +0530

    Time: 5 ms (27.81%), Space: 10.7 MB (35.72%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.cpp
+++ 0043-multiply-strings/0043-multiply-strings.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string multiply(string num1, string num2) {
-        if (num1 == "0" || num2 == "0") return "0";
+         if (num1 == "0" || num2 == "0") return "0";
         vector<int> res(num1.size() + num2.size(), 0);
         for (int i = num1.size()-1; i >= 0; i--){
             for (int j = num2.size()-1; j >= 0; j--){
@@ -20,5 +20,4 @@ public:
         }
         return ans;
     }
-
 };
