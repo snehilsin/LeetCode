8e06ca2fb22993fa1c19c6bdd2e239d0a172653f
snehilsin <110235521+snehilsin@users.noreply.github.com>
2025-03-25 19:03:50 +0530

    Time: 0 ms (100%), Space: 15.7 MB (67.7%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        // Break Point Method
-
+        // break point method
         // [2, 1, 5, 4, 3, 0, 0]
-        // bp --> 1
-        // just next > 1 is 3
+        // bp --> 1st element which is nums[i] < nums[i+1] , search for it from back
+
+        // bp = 1
+        // then find the next greater ele than 1 
+        // which is 3
         // swap 1 and 3
+        // [2, 3, 5, 4 ,1, 0, 0 ]
         // then reverse everything after 3
+        // [2, 3, 0, 0, 1, 4, 5]
 
         int n = nums.size();
         int bp = -1;
@@ -17,22 +21,21 @@ public:
                 break;
             }
         }
-
-        // if no bp, then reverse the whole array
+        // if no bp, reverse the whole array
         if (bp == -1){
             reverse(nums.begin(), nums.end());
             return;
         }
 
-        // find just next greater
+        // find next greater element
         for (int i = n-1; i >= 0; i--){
             if (nums[i] > nums[bp]){
                 swap(nums[i], nums[bp]);
                 break;
             }
         }
+        // then reverse everything after that bp
         reverse(nums.begin() + bp + 1, nums.end());
         return;
-
     }
 };
