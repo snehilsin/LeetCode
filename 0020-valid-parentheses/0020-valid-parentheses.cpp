2bb51da5436af6b672de204ce6ac58742a875ae7
snehilsin <110235521+snehilsin@users.noreply.github.com>
2025-03-27 12:29:02 +0530

    Time: 0 ms (100%), Space: 8.9 MB (35.96%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
-  
     bool isValid(string s) {
-       
+        //stack<char> st;
+        // in st, push opening parentheses
+       /*  unordered_map<char, char> mpp = { {')', '('}, {'}', '{'}, {']', '['}};
 
-       /* stack<char> st;
-        unordered_map<char, char> mpp = {
-            {')' , '('}, {'}', '{' }, {']', '['}
-        };
-             
         for (char c : s){
             if (mpp.find(c) == mpp.end()){
-                // opening parenthesis
+                // an opening parentheses
                 st.push(c);
             } else if (!st.empty() && mpp[c] == st.top()){
                 st.pop();
@@ -21,19 +17,17 @@ public:
         }
         return st.empty(); */
 
-       // st --> opening parentheses
-
-       stack<char> st;
-       for (char c : s){
-          if (c == '(' || c == '{' || c == '['){
-            st.push(c);
-          } else {
-            if (st.empty() || (c ==  ')' && st.top() != '(') || (c == '}' && st.top() != '{') || ( c == ']' && st.top() != '[')){
-                return false;
+        stack<char> st;
+        for (char c : s){
+            if ( c == '(' || c == '{' || c =='['){
+                st.push(c);
+            } else {
+                if (st.empty() || ( c == ')' && st.top() != '(') || (c == '}' && st.top() != '{') || ( c == ']' && st.top() != '[')){
+                    return false;
+                }
+                st.pop();
             }
-            st.pop();
-          }
-       }
-       return st.empty();
+        }
+        return st.empty();
     }
 };
