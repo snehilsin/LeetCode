34b2d065404a127d31c6e6adcfafe858c81dd566
snehilsin <110235521+snehilsin@users.noreply.github.com>
2025-04-26 23:12:57 +0530

    Time: 2 ms (57.62%), Space: 93.9 MB (62.97%) - LeetHub

--- 2527-count-subarrays-with-fixed-bounds/2527-count-subarrays-with-fixed-bounds.cpp
+++ 2527-count-subarrays-with-fixed-bounds/2527-count-subarrays-with-fixed-bounds.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
     long long countSubarrays(vector<int>& nums, int minK, int maxK) {
-        int n = nums.size();
-
-        int bad_idx = -1;  // it will store the values outside the range of [minK, maxK]
-        int left_idx , right_idx = -1;
-
+        int pMin = -1, pMax = -1, bad = -1;
         long long ans = 0;
-
-        for ( int i = 0; i < n; i++){
-            if ( nums[i] < minK || nums[i] > maxK){
-                bad_idx = i;
+        
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] == minK) pMin = i;
+            if (nums[i] == maxK) pMax = i;
+            if (nums[i] < minK || nums[i] > maxK) bad = i;
+            if (pMin != -1 && pMax != -1) {
+                ans += max(0, min(pMin, pMax) - bad);
             }
-            if ( nums[i] == minK){
-                left_idx = i;
-            }
-            if ( nums[i] == maxK){
-                right_idx = i;
-            }
-            int temp = min( left_idx, right_idx) - bad_idx;
-            ans += ( temp < 0) ? 0 : temp;
         }
         return ans;
     }
