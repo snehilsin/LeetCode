53d97d9423afb04d00962a41f4783e013ae37ab2
snehilsin <110235521+snehilsin@users.noreply.github.com>
2025-05-23 11:55:27 +0530

    Time: 2 ms (32.22%), Space: 97.6 MB (8.24%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,14 +1,31 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int maxPro = 0;
-        int mini = prices[0];
+        // brute force approach --> check every possible pair of buying and then selling day
 
-        for (int i = 1; i < prices.size(); i++){
-            int currProfit = prices[i] - mini;
-            maxPro= max(maxPro, currProfit);
-            mini = min(mini, prices[i]);
+/*
+        int maxProfit = 0;
+        int n = prices.size();
+
+        for ( int i = 0; i < n; i++){
+            for (int j = i+1; j < n; j++){
+                int profit = prices[j] - prices[i];
+                maxProfit = max(maxProfit, profit);
+            }
         }
-        return maxPro;
+        return maxProfit; */
+
+
+        // optimal approach -- keep track of min price as well as max profit
+
+       int maxPro = 0;
+       int mini = prices[0];
+
+       for (int i = 1; i < prices.size(); i++){
+        int currProfit = prices[i] - mini;
+        maxPro = max(maxPro, currProfit);
+        mini = min(mini, prices[i]);
+       }
+       return maxPro;
     }
 };
