55dc7498b1a14b3daed163dc350e39c9530c37cc
snehilsin <110235521+snehilsin@users.noreply.github.com>
2025-05-25 01:35:48 +0530

    Time: 0 ms (100%), Space: 12.4 MB (38.8%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -4,9 +4,8 @@ public:
         int i = m-1;
         int j = n-1;
         int k = m + n - 1;
-
-        while (j >= 0){
-            if (i >= 0 && nums1[i] > nums2[j]){
+        while ( j >= 0){
+            if (i >= 0&& nums1[i] >= nums2[j]){
                 nums1[k--] = nums1[i--];
             } else {
                 nums1[k--] = nums2[j--];
