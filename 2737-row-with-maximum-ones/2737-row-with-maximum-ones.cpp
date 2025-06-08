3fe31ee0ab4a3618b079d5127087e3ea08b313b9
snehilsin <110235521+snehilsin@users.noreply.github.com>
2025-06-08 08:30:02 -0700

    Time: 3 ms (65.03%), Space: 64.9 MB (14.07%) - LeetHub

--- 2737-row-with-maximum-ones/2737-row-with-maximum-ones.cpp
+++ 2737-row-with-maximum-ones/2737-row-with-maximum-ones.cpp
@@ -3,9 +3,9 @@ public:
     vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
         int cnt_max = 0;
         int index = 0;
-        for ( int i = 0; i < mat.size(); i++){
+        for (int i = 0; i < mat.size(); i++){
             int cnt_ones = 0;
-            for ( int j = 0; j < mat[0].size(); j++){
+            for (int j = 0; j < mat[0].size(); j++){
                 if (mat[i][j] == 1){
                     cnt_ones++;
                 }
