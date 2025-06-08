d0d800059cb28733bc83a1e27ec7aceb11e17c05
snehilsin <110235521+snehilsin@users.noreply.github.com>
2025-06-08 08:36:01 -0700

    Time: 0 ms (100%), Space: 13.3 MB (75.74%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -5,20 +5,20 @@ public:
         int rows = matrix.size();
         int cols = matrix[0].size();
         int low = 0;
-        int high = rows * cols-1;
+        int high = rows * cols -1;
         while (low <= high){
-             int mid = low + (high - low) / 2;
-             int row = mid / cols;
-             int col = mid % cols;
-             int guess = matrix[row][col];
+            int mid = low + (high - low) / 2;
+            int row = mid/cols;
+            int col = mid % cols;
 
-             if (guess == target){
+            int guess = matrix[row][col];
+            if (guess == target){
                 return true;
-             } else if (guess < target){
+            } else if (guess < target){
                 low = mid + 1;
-             } else {
+            } else {
                 high = mid -1;
-             }
+            }
         }
         return false;
     }
