3f1aecd60e1a0d5db66024527ef5697c9e8c5d43
snehilsin <110235521+snehilsin@users.noreply.github.com>
2025-07-30 23:01:16 -0700

    Time: 0 ms (100%), Space: 13.4 MB (42.14%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,41 +1,25 @@
 class Solution {
 public:
-    
-   /* bool binarySearch(vector<int> &nums, int target){
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
         int low = 0;
-        int high = nums.size()-1;
-        while (low <= high){
+        int high = rows*cols - 1;
+
+        while(low <= high){
             int mid = low + (high - low)/2;
-            if (nums[mid] == target) return true;
-            else if (nums[mid] < target) low = mid + 1;
-            else high = mid -1;
+            int row = mid / cols;
+            int col = mid % cols;
+
+            int guess = matrix[row][col];
+            if (guess == target){
+                return true;
+            } else if (guess < target){
+                low = mid + 1;
+            } else {
+                high = mid -1;
+            }
         }
         return false;
-    } */
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-       /* int n = matrix.size();
-        int m = matrix[0].size();
-       for (int i = 0; i < n; i++){
-        if (matrix[i][0] <= target && target <= matrix[i][m-1]){
-            return binarySearch(matrix[i], target);
-        }
-       }
-       return false; */
-       // formula
-       // imaginary conversion of 2d matrix to 1d array
-       // row = i/m, col = i % m
-       int n = matrix.size();
-       int m = matrix[0].size();
-       int low = 0;
-       int high = n*m -1;
-       while (low <= high){
-        int mid = low + (high - low)/2;
-        int row = mid/m;
-        int col = mid % m;
-        if (matrix[row][col] == target) return true;
-        else if (matrix[row][col] < target) low = mid + 1;
-        else high = mid -1;
-       }
-       return false;
     }
 };
