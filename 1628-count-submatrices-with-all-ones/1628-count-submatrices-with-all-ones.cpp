4d8bbd994c9a2e5c9e11ca3ef706c37682507411
snehilsin <110235521+snehilsin@users.noreply.github.com>
2025-08-20 23:02:33 -0700

    Time: 203 ms (12.93%), Space: 17.8 MB (89.59%) - LeetHub

---
+++ 1628-count-submatrices-with-all-ones/1628-count-submatrices-with-all-ones.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+
+    int helper( vector<vector<int>> &mat, int a , int b){
+        // submatrices with top-left corner at mat[a][b]
+
+        int m = mat.size();
+        int n = mat[0].size();
+        int count = 0;
+        int bound = n;
+        for ( int i = a; i < m; i++){
+            for ( int j = b; j < bound; j++){
+                if (mat[i][j]) count += 1;
+                else bound = j;
+
+            }
+        }
+        return count;
+    }
+    int numSubmat(vector<vector<int>>& mat) {
+        // bound --> to shrink the search space because if we hit a zero, then there is no point in iterating past that point
+        int m = mat.size();
+        int n = mat[0].size();
+
+        int count = 0;
+        for ( int i = 0; i < m; i++){
+            for ( int j = 0; j < n; j++){
+                count += helper(mat, i, j);
+            }
+        }
+        return count;
+    }
+};
