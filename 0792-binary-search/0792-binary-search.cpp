69d241dcb6f749511b0ed3bc6ace2a99c0afb614
snehilsin <110235521+snehilsin@users.noreply.github.com>
2025-09-06 07:34:00 -0700

    Time: 0 ms (100%), Space: 31.3 MB (79.85%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -4,12 +4,11 @@ public:
         int n = nums.size();
         int low = 0;
         int high = n-1;
-        while (low <= high){
-            int mid = low + (high - low) / 2;
-
+        while(low <= high){
+            int mid = low + (high-low)/2;
             if (nums[mid] == target){
                 return mid;
-            } 
+            }
             if (nums[mid] < target){
                 low = mid + 1;
             } else {
