8211870ed28532515039d3281083392b6bfa42b8
snehilsin <110235521+snehilsin@users.noreply.github.com>
2025-09-13 07:07:27 -0700

    Time: 0 ms (100%), Space: 28.1 MB (22.34%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -14,7 +14,7 @@ public:
     int maxPathDown(TreeNode* root, int &maxi){
         if (root == NULL) return 0;
         int left = max(0, maxPathDown(root->left, maxi));
-        int right = max(0, maxPathDown(root->right, maxi));
+        int right =  max(0, maxPathDown(root->right, maxi));
         maxi = max(maxi, left + right + root->val);
         return max(left, right) + root->val;
     }
