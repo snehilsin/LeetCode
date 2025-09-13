1f82a09c441319d4aebe7186e9a94cf5ed14cf52
snehilsin <110235521+snehilsin@users.noreply.github.com>
2025-09-12 22:36:19 -0700

    Time: 0 ms (100%), Space: 10.7 MB (83.41%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     bool helper(int  start, string s , vector<int> &memo, unordered_set<string> &wordSet){
         if (start == s.size()) return true;
@@ -19,5 +19,32 @@ public:
         vector<int> memo(n, -1);
         unordered_set<string> wordSet(wordDict.begin(), wordDict.end());
         return helper(0, s, memo, wordSet);
+    }
+};*/
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n = s.size();
+        unordered_set<string> wordSet(wordDict.begin(), wordDict.end());
+        int maxlen = 0;
+        for (auto &word : wordDict){
+            maxlen = max(maxlen , (int) word.size());
+        }
+
+        vector<bool> dp(n+1, false);
+        dp[0] = true;
+
+        for (int i = 1; i <= n; i++){
+            for (int len = 1; len <= maxlen && len <= i; len++){
+                if (!dp[i-len]) continue;
+
+                string word = s.substr(i-len, len);
+                if (wordSet.find(word) != wordSet.end() ){
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
     }
 };
