bdf89baea914f64fca2775e8c5e221c6d35dd4a7
snehilsin <110235521+snehilsin@users.noreply.github.com>
2026-07-29 07:27:56 -0700

    Time: 0 ms (100%), Space: 22.6 MB (53.86%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int i = 0; // ye pointer help karega unique elements store karne mein
-        // toh iss pointer ki help se in place modification karte hai
+       /* set<int>st;
+        for (int x : nums){
+            st.insert(x);
+        }
 
+        int i = 0;
+        for(int x : st){
+            nums[i] = x;
+            i++;
+        }
+        return st.size(); */
+
+        int i = 0;
         int n = nums.size();
-
-        for ( int j = 1; j < n; j++){
+        for (int j = 0; j < n; j++){
             if (nums[i] != nums[j]){
                 i++;
-                nums[i] = nums[j]; // unique hai toh store karlo na
             }
+            nums[i] = nums[j];
         }
-        return i + 1; // obviously kyunki tumne 0-based indexing jo ki, toh return i+1 (unique number of elements)
+        return i+1;
     }
 };
