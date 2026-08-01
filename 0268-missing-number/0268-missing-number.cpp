2f18beafe0b75ecff10b1d7575e321bc59a3be03
snehilsin <110235521+snehilsin@users.noreply.github.com>
2026-08-01 01:31:00 -0700

    Time: 3 ms (19.34%), Space: 21.6 MB (94.38%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        // Brute force approach 
-       /* int n = nums.size();
-        for (int i = 1; i <= n; i++){
+        // Brute Force Approach
+        /*int n = nums.size();
+        for(int i = 1; i <= n; i++){
             int flag = 0;
-            for (int j = 0; j < n; j++){
+            for(int j = 0; j < n; j++){
                 if (nums[j] == i){
                     flag = 1;
                     break;
@@ -17,39 +17,38 @@ public:
         }
         return -1; */
 
-        // Better Approach --> Hashing 
-       /* int n = nums.size();
+        // better approach --> hashing
+        /*int n = nums.size();
         vector<int> hash(n+1, 0);
-        for (int i = 0; i < n; i++){
+        for(int i = 0; i < n; i++){
             hash[nums[i]]++;
         }
-        for (int i = 0; i < n; i++){
+
+        for(int i = 0; i < n; i++){
             if (hash[i] == 0){
                 return i;
             }
         }
         return -1; */
-          
-        // optimal approach 1 --> summation approach
+
+        // optimal approach -> summation approach
         int n = nums.size();
-        // total sum
-        int total_sum = (n*(n+1))/2;
+        int total_sum = (n * (n+1)) / 2;
         int sum = 0;
-        for ( int i = 0; i < n; i++){
-            sum+= nums[i];
+        for(int i = 0; i < n; i++){
+            sum += nums[i];
         }
-       return total_sum - sum; 
+        return total_sum - sum; 
 
-
-        // optimal approach 2 - XOR
+        // optimal approach -> XOR
        /* int xor1 = 0; // xor of all array elements
         int xor2 = 0; // xor of numbers from 1 to n
         int n = nums.size();
-        for (int i = 0; i < n; i++){
+        for(int i = 0; i < n; i++){
             xor1 = xor1 ^ nums[i];
             xor2 = xor2 ^ (i+1);
         }
         xor2 = xor2 ^ n;
-        return xor1 ^ xor2; */
+        return xor1 ^ xor2;*/
     }
 };
